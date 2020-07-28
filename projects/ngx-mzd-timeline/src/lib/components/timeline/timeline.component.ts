a46c70094320630c476dbcbeef5bf77102d82f72
Rend <rendtm@gmail.com>
2020-07-28 14:52:17 +0200

    fixed side can now be used in alternate Mode

--- projects/ngx-mzd-timeline/src/lib/components/timeline/timeline.component.ts
+++ projects/ngx-mzd-timeline/src/lib/components/timeline/timeline.component.ts
@@ -14,42 +14,45 @@ export class MzdTimelineComponent implements OnInit, AfterViewInit, OnChanges {
 
   @Input() firstContentSide: 'left' | 'right' = 'left';
   @Input() alternateSide: boolean = true;
-  @ContentChildren(MzdTimelineContentComponent) contents: QueryList<MzdTimelineContentComponent>;
+  @ContentChildren(MzdTimelineContentComponent) contents: QueryList<MzdTimelineContentComponent> = [] as any;
 
   constructor() { }
 
   ngOnChanges(changes: SimpleChanges): void {
-    this.updateContent();
+    this.ngAfterViewInit();
   }
 
   ngOnInit(): void { }
 
   ngAfterViewInit(): void {
-    this.updateContent();
+    if (this.alternateSide) {
+      this.handleAlternateSide();
+    } else {
+      this.handleOneSide();
+    }
+    this.contents.forEach(content => content.noAlternate = !this.alternateSide);
   }
 
-  private updateContent(): void {
-    if (this.contents) {
-      if (this.alternateSide) {
-        if (this.firstContentSide === 'left') {
-          this.contents.forEach((content, index) => {
-            content.left = (index % 2 === 0);
-            content.right = (index % 2 !== 0);
-          });
-        } else {
-          this.contents.forEach((content, index) => {
-            content.left = (index % 2 !== 0);
-            content.right = (index % 2 === 0);
-          });
-        }
+  private handleAlternateSide() {
+    const alternateSideDecider = this.firstContentSide === 'left' ? 0 : 1;
+
+    this.contents.forEach((content, index) => {
+      if (content.side === undefined) {
+        content.left = (index % 2 === alternateSideDecider);
+        content.right = !(index % 2 === alternateSideDecider);
       } else {
-        this.contents.forEach(content => {
-          content.left = this.firstContentSide === 'left';
-          content.right = this.firstContentSide === 'right';
-        });
+        content.left = content.side === 'left';
+        content.right = content.side === 'right';
       }
-      this.contents.forEach(content => content.noAlternate = !this.alternateSide);
-    }
+
+    });
+  }
+
+  private handleOneSide() {
+    this.contents.forEach(content => {
+      content.left = this.firstContentSide === 'left';
+      content.right = this.firstContentSide === 'right';
+    });
   }
 
 }
